ed4941a31bdb5262fe692c9d74b4c62eb9966939
Macarena Capra Prince <162723190+macaprynce@users.noreply.github.com>
2024-05-30 22:49:24 -0300

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -2,31 +2,24 @@
 
 
 def get_coordinate(record):
-    """Return coordinate value from a tuple containing the treasure name, and treasure coordinate.
-
-    :param record: tuple - with a (treasure, coordinate) pair.
-    :return: str - the extracted map coordinate.
-    """
-
-    return ""
-
+    return record[1]
 
 def convert_coordinate(coordinate):
-    """Split the given coordinate into tuple containing its individual components.
-
-    :param coordinate: str - a string map coordinate
-    :return: tuple - the string coordinate split into its individual components.
-    """
-
-    return ()
+    num = coordinate [0]
+    let= coordinate[1]
+    return (num, let)
 
 
 def create_record(azara_record, rui_record):
-    """Combine the two record types (if possible) and create a combined record group.
+    a, b= azara_record
+    c, d, e= rui_record
+    num= b[0]
+    let= b[1]
+    coor_az = (num, let)
 
-    :param azara_record: tuple - a (treasure, coordinate) pair.
-    :param rui_record: tuple - a (location, coordinate, quadrant) trio.
-    :return: tuple or str - the combined record (if compatible), or the string "not a match" (if incompatible).
-    """
+    if coor_az== d:
+        final = azara_record + rui_record
+    else:
+        final = "not a match"
 
-    return ()
+    return final
